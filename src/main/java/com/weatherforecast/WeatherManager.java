1f9236e6d53c0cfd36350bfa256c2011cbbc2f50
Arkadiusz Kawalec <arkadiusz.kawalec.programista@gmail.com>
2020-04-24 21:07:14 +0200

    Receiving data from OWMjapis in WeatherForecast.

--- src/main/java/com/weatherforecast/WeatherManager.java
+++ src/main/java/com/weatherforecast/WeatherManager.java
@@ -1,7 +1,32 @@
 package com.weatherforecast;
 
+import com.weatherforecast.model.WeatherForecast;
+import net.aksingh.owmjapis.api.APIException;
+
 /**
  * Created by Arek on 19.04.2020.
  */
 public class WeatherManager {
+
+    public WeatherManager() throws APIException {
+    }
+
+    public void showForecast() throws APIException {
+        String cityName = "London, UK";
+        WeatherForecast weatherForecast = new WeatherForecast(cityName);
+
+        System.out.println(weatherForecast .getCityName() + " ," + weatherForecast .getCountryCode());
+
+        for (int i = 0; i < weatherForecast .getForecastSize(); i++ ){
+            System.out.println("");
+            System.out.println(i);
+            System.out.println(weatherForecast .getDateTimeText(i));
+            System.out.println(weatherForecast .getTemp(i));
+            System.out.println(weatherForecast .getHumidity(i));
+            System.out.println(weatherForecast .getDescription(i));
+            System.out.println(weatherForecast .getIconLink(i));
+            System.out.println(weatherForecast .getWindSpeed(i));
+        }
+    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,13 @@
 module com.weatherforecast {
     requires javafx.controls;
     requires javafx.fxml;
+    requires owm.japis;
+    requires java.sql;
 
     opens com.weatherforecast to javafx.fxml;
     opens com.weatherforecast.controller to javafx.fxml;
     opens com.weatherforecast.view to javafx.fxml;
+    opens com.weatherforecast.model to javafx.fxml;
 
     exports com.weatherforecast;
 }
