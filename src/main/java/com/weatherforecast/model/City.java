b026fcb6805aeab80253ecc5c755b44565b2da66
Arkadiusz Kawalec <arkadiusz.kawalec.programista@gmail.com>
2020-04-27 19:00:20 +0200

    Refactorization, current weather forecast.

--- src/main/java/com/weatherforecast/model/City.java
+++ src/main/java/com/weatherforecast/model/City.java
@@ -1,23 +1,15 @@
 package com.weatherforecast.model;
 
 public class City {
-    private int id;
     private String name;
     private String country;
 
     public City(int id, String name, String country) {
-        this.id = id;
         this.name = name;
         this.country = country;
     }
 
-    public int getCityId() {
-
-        return id;
-    }
-
     public String getCityName() {
-
         return name;
     }
 
