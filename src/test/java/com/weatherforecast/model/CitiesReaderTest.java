8fbe67df21016b7a02e395a4166c06d3b7426a13
Arkadiusz Kawalec <arkadiusz.kawalec.programista@gmail.com>
2020-05-17 22:03:52 +0200

    Add directories and libraries for tests.

---
+++ src/test/java/com/weatherforecast/model/CitiesReaderTest.java
@@ -0,0 +1,15 @@
+package com.weatherforecast.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Arek on 17.05.2020.
+ */
+class CitiesReaderTest {
+
+
+}
