8c45dab883273218a06b54426e0388408a9b7918
Arkadiusz Kawalec <arkadiusz.kawalec.programista@gmail.com>
2020-05-20 19:59:01 +0200

    End WeatherTest class.

--- src/test/java/com/weatherforecast/model/WeatherTest.java
+++ src/test/java/com/weatherforecast/model/WeatherTest.java
@@ -75,5 +75,121 @@ class WeatherTest{
         assertThat(weather.getDateWithoutTime(0), is(notNullValue()));
     }
 
+    @Test
+    void dateWithoutTimeLengthShouldBeEqualTo10() {
 
+        //given
+        //when
+        String dateWithoutTime = weather.getDateWithoutTime(0);
+        int length = dateWithoutTime.length();
+
+        //then
+        assertThat(length, equalTo(10));
+    }
+
+    @Test
+    void timeWithoutDateShouldNotBeNull() {
+        //when
+        //then
+        assertThat(weather.getTimeWithoutDate(0), is(notNullValue()));
+    }
+
+    @Test
+    void timeWithoutDateLengthShouldBeEqualTo8() {
+
+        //given
+        //when
+        String timeWithoutDate = weather.getTimeWithoutDate(0);
+        int length = timeWithoutDate .length();
+
+        //then
+        assertThat(length, equalTo(8));
+    }
+
+    @Test
+    void temperatureShouldNotBeNull() {
+        //when
+        //then
+        assertThat(weather.getTemp(0), is(notNullValue()));
+    }
+
+    @Test
+    void humidityShouldNotBeNull() {
+        //when
+        //then
+        assertThat(weather.getHumidity(0), is(notNullValue()));
+    }
+
+    @Test
+    void descriptionShouldNotBeNull() {
+        //when
+        //then
+        assertThat(weather.getDescription(0), is(notNullValue()));
+    }
+
+    @Test
+    void mainWeatherShouldNotBeNull() {
+        //when
+        //then
+        assertThat(weather.getMainWeather(0), is(notNullValue()));
+    }
+
+    @Test
+    void iconLinkShouldNotBeNull() {
+        //when
+        //then
+        assertThat(weather.getIconLink(0), is(notNullValue()));
+    }
+
+
+    @Test
+    void equalsMethodShouldReturnTrueWhenTwoWeathersAreTheSame() throws APIException {
+        //given
+        Weather weather2 = weatherForecastFetcher.getWeatherForecast("London, GB");
+
+        //when
+        //then
+        assertThat(weather.equals(weather2), is(true));
+    }
+
+    @Test
+    void equalsMethodShouldReturnFalseWhenTwoWeathersAreNotTheSame() throws APIException {
+        //given
+        Weather weather2 = weatherForecastFetcher.getWeatherForecast("Warsaw, PL");
+
+        //when
+        //then
+        assertThat(weather.equals(weather2), is(false));
+    }
+
+    @Test
+    void equalsMethodShouldReturnFalseWhenTheClassTypesAreDifferent() {
+        //when
+        //then
+        assertThat(weather.equals(new Object()), is(false));
+    }
+
+    @Test
+    void comparingHashCodeMethodShouldReturnTrueWhenTwoWeathersAreTheSame() throws APIException {
+        //given
+        Weather weather2 = weatherForecastFetcher.getWeatherForecast("London, GB");
+
+        //when
+        boolean isHashcodeEquals = weather.hashCode() == weather2.hashCode();
+
+        //then
+        assertThat(isHashcodeEquals, is(true));
+    }
+
+    @Test
+    void comparingHashCodeMethodShouldReturnFalseWhenTwoWeathersAreNotTheSame() throws APIException {
+        //given
+        Weather weather2 = weatherForecastFetcher.getWeatherForecast("Warsaw, PL");
+
+        //when
+        boolean isHashcodeEquals = weather.hashCode() == weather2.hashCode();
+
+        //then
+        assertThat(isHashcodeEquals, is(false));
+    }
 }
